67355bb7a338a2980855ed333fd82844dc79eb1f
unknown <raju.27072001@gmail.com>
2022-05-23 03:23:22 +0530

    Initial commit

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 const dirPath = path.join(__dirname, '/public');
-
 const static = app.use(express.static(dirPath));
+
+const characters = require('./routes/characters');
+const character = require('./routes/characters');
+const students = require('./routes/students');
+const teachers = require('./routes/teachers');
 
 app.get('/', (req, res) => {
     res.sendFile(static + '/index.html');
 });
 
-app.use('/api/characters', require('./routes/characters'));
+app.use('/api/characters', characters);
 
-app.use('/api/character', require('./routes/characters'));
+app.use('/api/character', character);
 
-app.use('/api/students', require('./routes/students'));
+app.use('/api/students', students);
 
-app.use('/api/teachers', require('./routes/teachers'));
+app.use('/api/teachers', teachers);
 
 app.listen(PORT, () => console.log(`Server is up on http://localhost:${PORT}`));
